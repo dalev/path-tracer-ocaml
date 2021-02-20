ca45943ef1870002151290df4c3bab822e2561f5
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-02-20 17:15:17 -0500

    bench sphere intersect

--- bench/bench.ml
+++ bench/bench.ml
@@ -4,6 +4,10 @@ open Path_tracer
 
 let unit_bbox = Bbox.create ~min:P3.origin ~max:(P3.create ~x:1.0 ~y:1.0 ~z:1.0)
 
+let unit_sphere =
+  let material = Material.lambertian (Texture.solid Color.black) in
+  Shape.sphere ~material ~center:P3.origin ~radius:1.0
+
 let ray_hit, ray_miss =
   let o = P3.create ~x:(-5.0) ~y:0.5 ~z:0.5 in
   let hit = V3.unit_x in
@@ -16,10 +20,18 @@ let t_max = 10.0
 let is_hit ray =
   Bbox.is_hit (Sys.opaque_identity unit_bbox) (Sys.opaque_identity ray) ~t_min ~t_max
 
+let shape_intersect s ray =
+  Shape.intersect (Sys.opaque_identity s) (Sys.opaque_identity ray) ~t_min ~t_max
+  |> Option.is_some
+
 let () =
   Core.Command.run
     (Bench.make_command
        [ Bench.Test.create_group ~name:"bbox"
            [ Bench.Test.create ~name:"hit" (fun () -> assert (is_hit ray_hit))
            ; Bench.Test.create ~name:"miss" (fun () -> assert (not (is_hit ray_miss))) ]
-       ] )
+       ; Bench.Test.create_group ~name:"sphere"
+           [ Bench.Test.create ~name:"hit" (fun () ->
+                 assert (shape_intersect unit_sphere ray_hit) )
+           ; Bench.Test.create ~name:"miss" (fun () ->
+                 assert (not (shape_intersect unit_sphere ray_miss)) ) ] ] )
