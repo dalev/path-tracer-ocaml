3b2e894a43c638fc1f37f057cfd22a21d3dd5531
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-02-20 20:18:34 -0500

    monomorphize V3 and P3

--- lib/affine.ml
+++ lib/affine.ml
@@ -1,38 +1,29 @@
 open Base
 
-type 'a poly = 'a * 'a * 'a
-type p3 = float poly
+type p3 = {x: float; y: float; z: float}
 
-let to_string (x, y, z) = Printf.sprintf "(%f, %f, %f)" x y z
+let to_string {x; y; z} = Printf.sprintf "(%f, %f, %f)" x y z
 let pp f t = Caml.Format.pp_print_string f (to_string t)
 
 module V3 = struct
-  type t = float poly
+  type t = p3
 
   let pp = pp
-  let create ~x ~y ~z = (x, y, z)
-  let coords = Fn.id
-  let x (x, _, _) = x
-  let y (_, y, _) = y
-  let z (_, _, z) = z
-  let yzx (x, y, z) = (y, z, x)
+  let create ~x ~y ~z : t = {x; y; z}
+  let coords {x; y; z} = (x, y, z)
+  let x t = t.x
+  let y t = t.y
+  let z t = t.z
+  let yzx {x; y; z} = {x= y; y= z; z= x}
   let unit_x = create ~x:1.0 ~y:0.0 ~z:0.0
   let unit_y = create ~x:0.0 ~y:1.0 ~z:0.0
   let unit_z = create ~x:0.0 ~y:0.0 ~z:1.0
-  let axis (x, y, z) a = match (a : Axis.t) with X -> x | Y -> y | Z -> z
-
-  module A = Applicative.Make (struct
-    type 'a t = 'a poly
-
-    let return a = (a, a, a)
-    let apply (f, g, h) (a, b, c) = (f a, g b, h c)
-    let map = `Custom (fun (a, b, c) ~f -> (f a, f b, f c))
-  end)
-
-  include A
-
-  let zero = return 0.0
-  let one = return 1.0
+  let axis {x; y; z} a = match (a : Axis.t) with X -> x | Y -> y | Z -> z
+  let map {x; y; z} ~f = {x= f x; y= f y; z= f z}
+  let map2 a b ~f = {x= f a.x b.x; y= f a.y b.y; z= f a.z b.z}
+  let of_float a = {x= a; y= a; z= a}
+  let zero = of_float 0.0
+  let one = of_float 1.0
 
   module Infix = struct
     let ( + ) = map2 ~f:( +. )
@@ -42,18 +33,17 @@ module V3 = struct
     let ( ~- ) = map ~f:( ~-. )
   end
 
-  let of_float x = return x
   let of_points ~src ~tgt = Infix.( - ) tgt src
 
   (* These two inlines are to avoid alloc in Bbox.is_hit *)
-  let[@inline] min_coord (x, y, z) = Float.min x (Float.min y z)
-  let[@inline] max_coord (x, y, z) = Float.max x (Float.max y z)
+  let[@inline] min_coord {x; y; z} = Float.min x (Float.min y z)
+  let[@inline] max_coord {x; y; z} = Float.max x (Float.max y z)
 
   (* This inline is to reduce alloc in sphere intersect *)
   let[@inline] dot v w =
     let open Infix in
-    let a, b, c = v * w in
-    a +. b +. c
+    let {x; y; z} = v * w in
+    x +. y +. z
 
   let[@inline] scale v s = map ~f:(( *. ) s) v
   let[@inline] quadrance v = dot v v
@@ -63,8 +53,8 @@ module V3 = struct
     let scalar = 1.0 /. Float.sqrt (quadrance v) in
     scale v scalar
 
-  let cross (a, b, c) (d, e, f) =
-    ((b *. f) -. (c *. e), (c *. d) -. (a *. f), (a *. e) -. (b *. d))
+  let cross {x= a; y= b; z= c} {x= d; y= e; z= f} =
+    {x= (b *. f) -. (c *. e); y= (c *. d) -. (a *. f); z= (a *. e) -. (b *. d)}
 end
 
 module P3 = struct
