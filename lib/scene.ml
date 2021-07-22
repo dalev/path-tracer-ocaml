c4c58d14b6cad150058e7af5ac6d352989fefe1f
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-07-22 17:31:21 -0400

    trim intf

--- lib/scene.ml
+++ lib/scene.ml
@@ -9,13 +9,6 @@ let create camera shapes ~background =
     |> Bvh.create in
   {bvh; background; camera}
 
-let camera_ray t cx cy = Camera.ray t.camera cx cy
-let background t ray = t.background ray
-
-let intersect t ray =
-  let t_min = 0.0 and t_max = Float.max_finite_value in
-  Bvh.intersect t.bvh ray ~t_min ~t_max
-
 let diffuse_plus_light_pdf (_ : t) =
   (* CR dalev: area lights *)
   Pdf.diffuse
--- lib/scene.mli
+++ lib/scene.mli
@@ -3,10 +3,6 @@ open Base
 type t
 
 val create : Camera.t -> Shape.t list -> background:(Ray.t -> Color.t) -> t
-val camera_ray : t -> float -> float -> Ray.t
-val background : t -> Ray.t -> Color.t
-val intersect : t -> Ray.t -> Hit.t option
-val diffuse_plus_light_pdf : t -> Pdf.t
 
 val path_tracer :
      t
