0aa97b87ba31297840d21212976dc2c1bbe58a43
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-08-03 09:27:45 -0400

    .

--- low_discrepancy_sequence/test/low_discrepancy_sequence_test.ml
+++ low_discrepancy_sequence/test/low_discrepancy_sequence_test.ml
@@ -12,19 +12,22 @@ let integrate_1d f lower upper ~iterations =
     l := l';
     scale_sample ~lower ~upper @@ L.Sample.(s.%{0})
   in
-  let neg_sum = ref 0.0
-  and pos_sum = ref 0.0 in
+  let sum = ref 0.0
+  and c = ref 0.0 in
   for _ = 0 to iterations - 1 do
-    let x = sample () in
-    let y = f x in
-    if y < 0.0 then neg_sum := !neg_sum +. y else pos_sum := !pos_sum +. y
+    let y = (f @@ sample ()) -. !c in
+    let t = !sum +. y in
+    c := t -. !sum -. y;
+    sum := t
   done;
-  let sum = !pos_sum +. !neg_sum in
   let v_over_n = (upper -. lower) /. float_of_int iterations in
-  v_over_n *. sum
+  v_over_n *. !sum
 ;;
 
-let check_float = Alcotest.(check (float 1e-3)) "within 1e-3"
+let check_float ~within =
+  let msg = "within " ^ string_of_float within in
+  Alcotest.(check (float within)) msg
+;;
 
 let () =
   let test_case = Alcotest.test_case in
@@ -32,20 +35,30 @@ let () =
     "low_discrepancy_sequence"
     [ ( "scale-sample"
       , let scale_sample s = scale_sample s ~lower:(-2.0) ~upper:3.0 in
+        let within = 1e-3 in
         [ test_case "scale_sample 0 -> lower" `Quick (fun () ->
-              check_float (-2.0) (scale_sample 0.0))
+              check_float ~within (-2.0) (scale_sample 0.0))
         ; test_case "scale_sample 1 -> upper" `Quick (fun () ->
-              check_float 3.0 (scale_sample 1.0))
+              check_float ~within 3.0 (scale_sample 1.0))
         ] )
     ; ( "1D-integrals"
-      , [ test_case "sin(x) from 0 to 1" `Quick (fun () ->
-              check_float 2.0 (integrate_1d sin 0.0 Float.pi ~iterations:1_000))
-        ; test_case "sin(x) from -1 to 1" `Quick (fun () ->
-              check_float 0.0 (integrate_1d sin (-1.0) 1.0 ~iterations:5_000))
-        ; test_case "quarter circle -> pi/4" `Quick (fun () ->
+      , [ test_case "sin(x) from 0 to pi = 2" `Quick (fun () ->
               check_float
+                ~within:1e-3
+                2.0
+                (integrate_1d sin 0.0 Float.pi ~iterations:1_000))
+        ; test_case "sin(x) from -1 to 1 = 0" `Quick (fun () ->
+              check_float ~within:1e-3 0.0 (integrate_1d sin (-1.0) 1.0 ~iterations:5_000))
+        ; test_case "quarter circle = pi/4" `Quick (fun () ->
+              check_float
+                ~within:1e-3
                 (Float.pi /. 4.0)
                 (integrate_1d (fun x -> sqrt (1.0 -. (x *. x))) 0.0 1.0 ~iterations:2_000))
+        ; test_case "exp(x) from 0 to 3 = e^3 - 1" `Quick (fun () ->
+              check_float
+                ~within:0.03
+                (Float.expm1 3.0)
+                (integrate_1d Float.exp 0.0 3.0 ~iterations:2_000))
         ] )
     ]
 ;;
