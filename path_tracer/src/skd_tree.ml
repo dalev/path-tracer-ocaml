0c447820a51ea63f37236d687a6dbbae8c06320a
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-08-07 13:16:41 -0400

    Lift helpers out of functor by making Bshape.t and Proposal.t polymorphic.

--- path_tracer/src/skd_tree.ml
+++ path_tracer/src/skd_tree.ml
@@ -1,30 +1,128 @@
 open Base
 include Skd_tree_intf
 
+let num_bins = 32
+
+module Bshape = struct
+  type 'a t =
+    { shape : 'a
+    ; bbox : Bbox.t
+    ; centroid : P3.t
+    }
+
+  let bbox t = t.bbox
+  let cbox t = Bbox.create ~min:t.centroid ~max:t.centroid
+  let centroid t = t.centroid
+  let shape t = t.shape
+
+  let create elt_bbox shape =
+    let bbox = elt_bbox shape in
+    let centroid = Bbox.center bbox in
+    { shape; bbox; centroid }
+  ;;
+
+  let centroid_bbox bshapes = Slice.map_reduce bshapes ~transform:cbox ~combine:Bbox.union
+end
+
+module Bin = struct
+  type t =
+    { count : int
+    ; bounds : Bbox.t option
+    }
+
+  let create () = { count = 0; bounds = None }
+  let bbox t = t.bounds
+
+  let scaled_area t =
+    Option.map t.bounds ~f:(fun bbox -> Float.of_int t.count *. Bbox.surface_area bbox)
+  ;;
+
+  let insert t b =
+    let b_box = Bshape.bbox b in
+    let bounds =
+      Some
+        (match t.bounds with
+        | None -> b_box
+        | Some t_box -> Bbox.union t_box b_box)
+    in
+    { count = t.count + 1; bounds }
+  ;;
+
+  let join_bounds t t' = Bbox.union_opt t.bounds t'.bounds
+
+  let join t t' =
+    let bounds = join_bounds t t' in
+    { count = t.count + t'.count; bounds }
+  ;;
+
+  let join_slice = Slice.reduce_exn ~f:join
+end
+
+module Proposal = struct
+  type 'a t =
+    { cost : float
+    ; split_index : int
+    ; axis : Axis.t
+    ; on_lhs : 'a Bshape.t -> bool
+    }
+
+  let costI = 1.0
+  let costT = 0.25
+  let leaf_cost n = costI *. Float.of_int n
+  let cost t = t.cost
+  let on_lhs t = t.on_lhs
+  let axis t = t.axis
+
+  let candidates bins to_bin axis =
+    let total_bbox =
+      Array.filter_map bins ~f:Bin.bbox |> Array.reduce_exn ~f:Bbox.union
+    in
+    let total_area = Bbox.surface_area total_bbox in
+    let bins = Slice.create bins in
+    let f = Fn.compose Bin.scaled_area Bin.join_slice in
+    List.init (num_bins - 1) ~f:(fun p ->
+        let lhs, rhs = Slice.split_at bins (p + 1) in
+        match f lhs, f rhs with
+        | None, _ | _, None -> None
+        | Some lhs_area, Some rhs_area ->
+          let on_lhs b = to_bin b <= p in
+          let open Float.O in
+          let cost = costT + ((lhs_area + rhs_area) * costI / total_area) in
+          Some { cost; split_index = p; axis; on_lhs })
+    |> List.filter_opt
+  ;;
+
+  let compare p1 p2 = Float.compare (cost p1) (cost p2)
+
+  let propose_split_one_axis shapes axis cbox =
+    let to_axis = P3.axis axis in
+    let to_bin =
+      let epsilon = 1e-6 in
+      let cb_min = to_axis (Bbox.min cbox) in
+      let cb_max = to_axis (Bbox.max cbox) in
+      let scale = Float.of_int num_bins *. (1.0 -. epsilon) /. (cb_max -. cb_min) in
+      fun b -> Float.to_int (scale *. (to_axis (Bshape.centroid b) -. cb_min))
+    in
+    let bins = Array.init num_bins ~f:(fun (_ : int) -> Bin.create ()) in
+    Slice.iter shapes ~f:(fun s ->
+        let j = to_bin s in
+        let bin = bins.(j) in
+        bins.(j) <- Bin.insert bin s);
+    candidates bins to_bin axis |> List.min_elt ~compare
+  ;;
+
+  let create shapes =
+    let cbbox = Bshape.centroid_bbox shapes in
+    let candidates =
+      List.filter_map
+        Axis.[ X; Y; Z ]
+        ~f:(fun axis -> propose_split_one_axis shapes axis cbbox)
+    in
+    List.min_elt candidates ~compare
+  ;;
+end
+
 module Make (L : Leaf) : S with type elt := L.elt and type elt_hit := L.elt_hit = struct
-  module Bshape = struct
-    type t =
-      { shape : L.elt
-      ; bbox : Bbox.t
-      ; centroid : P3.t
-      }
-
-    let bbox t = t.bbox
-    let cbox t = Bbox.create ~min:t.centroid ~max:t.centroid
-    let centroid t = t.centroid
-    let shape t = t.shape
-
-    let create shape =
-      let bbox = L.elt_bbox shape in
-      let centroid = Bbox.center bbox in
-      { shape; bbox; centroid }
-    ;;
-
-    let centroid_bbox bshapes =
-      Slice.map_reduce bshapes ~transform:cbox ~combine:Bbox.union
-    ;;
-  end
-
   type tree =
     | Leaf of L.t
     | Branch of
@@ -117,117 +215,16 @@ module Make (L : Leaf) : S with type elt := L.elt and type elt_hit := L.elt_hit 
     intersect_tree t.tree ray ~t_enter ~t_leave
   ;;
 
-  (* == Tree builder using Binned SAH == *)
-
-  module Bin = struct
-    type t =
-      { count : int
-      ; bounds : Bbox.t option
-      }
-
-    let create () = { count = 0; bounds = None }
-    let bbox t = t.bounds
-
-    let scaled_area t =
-      Option.map t.bounds ~f:(fun bbox -> Float.of_int t.count *. Bbox.surface_area bbox)
-    ;;
-
-    let insert t b =
-      let b_box = Bshape.bbox b in
-      let bounds =
-        Some
-          (match t.bounds with
-          | None -> b_box
-          | Some t_box -> Bbox.union t_box b_box)
-      in
-      { count = t.count + 1; bounds }
-    ;;
-
-    let join_bounds t t' = Bbox.union_opt t.bounds t'.bounds
-
-    let join t t' =
-      let bounds = join_bounds t t' in
-      { count = t.count + t'.count; bounds }
-    ;;
-  end
-
-  let join_bins = Slice.reduce_exn ~f:Bin.join
-  let num_bins = 32
-  let costI = 1.0
-  let costT = 0.25
-  let leaf_cost n = costI *. Float.of_int n
-
-  module Proposal = struct
-    type t =
-      { cost : float
-      ; split_index : int
-      ; axis : Axis.t
-      ; on_lhs : Bshape.t -> bool
-      }
-
-    let cost t = t.cost
-    let on_lhs t = t.on_lhs
-    let axis t = t.axis
-
-    let candidates bins to_bin axis =
-      let total_bbox =
-        Array.filter_map bins ~f:Bin.bbox |> Array.reduce_exn ~f:Bbox.union
-      in
-      let total_area = Bbox.surface_area total_bbox in
-      let bins = Slice.create bins in
-      let f = Fn.compose Bin.scaled_area join_bins in
-      List.init (num_bins - 1) ~f:(fun p ->
-          let lhs, rhs = Slice.split_at bins (p + 1) in
-          match f lhs, f rhs with
-          | None, _ | _, None -> None
-          | Some lhs_area, Some rhs_area ->
-            let on_lhs b = to_bin b <= p in
-            let open Float.O in
-            let cost = costT + ((lhs_area + rhs_area) * costI / total_area) in
-            Some { cost; split_index = p; axis; on_lhs })
-      |> List.filter_opt
-    ;;
-
-    let compare p1 p2 = Float.compare (cost p1) (cost p2)
-  end
-
-  let propose_split_one_axis shapes axis cbox =
-    let to_axis = P3.axis axis in
-    let to_bin =
-      let epsilon = 1e-6 in
-      let cb_min = to_axis (Bbox.min cbox) in
-      let cb_max = to_axis (Bbox.max cbox) in
-      let scale = Float.of_int num_bins *. (1.0 -. epsilon) /. (cb_max -. cb_min) in
-      fun b -> Float.to_int (scale *. (to_axis (Bshape.centroid b) -. cb_min))
-    in
-    let bins = Array.init num_bins ~f:(fun (_ : int) -> Bin.create ()) in
-    Slice.iter shapes ~f:(fun s ->
-        let j = to_bin s in
-        let bin = bins.(j) in
-        bins.(j) <- Bin.insert bin s);
-    Proposal.candidates bins to_bin axis |> List.min_elt ~compare:Proposal.compare
-  ;;
-
-  let propose_split shapes =
-    let cbbox = Bshape.centroid_bbox shapes in
-    let candidates =
-      List.filter_map
-        Axis.[ X; Y; Z ]
-        ~f:(fun axis -> propose_split_one_axis shapes axis cbbox)
-    in
-    List.min_elt candidates ~compare:Proposal.compare
-  ;;
-
   let make_leaf shapes = Leaf (L.of_elts (Slice.to_array_map shapes ~f:Bshape.shape))
 
   let rec create_tree shapes =
     if Slice.length shapes <= L.length_cutoff
     then make_leaf shapes
     else (
-      match propose_split shapes with
+      match Proposal.create shapes with
       | None -> make_leaf shapes
       | Some p ->
-        let leaf_cost = leaf_cost (Slice.length shapes) in
+        let leaf_cost = Proposal.leaf_cost (Slice.length shapes) in
         let open Float.O in
         if Proposal.cost p <= leaf_cost
         then (
@@ -254,7 +251,7 @@ module Make (L : Leaf) : S with type elt := L.elt and type elt_hit := L.elt_hit 
     if List.is_empty elts then failwith "Skd_tree.create: given empty list of shapes";
     let elts =
       Sequence.of_list elts
-      |> Sequence.map ~f:Bshape.create
+      |> Sequence.map ~f:(Bshape.create L.elt_bbox)
       |> Sequence.to_array
       |> Slice.create
     in
