226d0f4bef0419c1733f91400a2c1b67b2710319
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-08-15 11:01:09 -0400

    add spheres

--- cornell-box/bin/main.ml
+++ cornell-box/bin/main.ml
@@ -42,30 +42,106 @@ let quad ~material a u v =
   triangle_fan ~material [ a; b; c; d ]
 ;;
 
+let solid_tex r g b = Texture.solid (Color.create ~r ~g ~b)
+
 let empty_box () =
-  let solid r g b = Material.lambertian (Texture.solid (Color.create ~r ~g ~b)) in
+  let solid r g b = Material.lambertian (solid_tex r g b) in
   let red = solid 0.7 0.0 0.0 in
   let blue = solid 0.0 0.0 0.7 in
   let grey = solid 0.7 0.7 0.7 in
+  let checker =
+    let a = solid_tex 0.2 0.3 0.1 in
+    let b = solid_tex 0.9 0.9 0.9 in
+    Material.lambertian @@ Texture.checker ~width:10 ~height:10 a b
+  in
   let right_wall = quad ~material:red P3.origin V3.unit_z V3.unit_y in
   let left_wall = quad ~material:blue (P3.of_v3 V3.unit_x) V3.unit_z V3.unit_y in
-  let floor = quad ~material:grey P3.origin V3.unit_x V3.unit_z in
+  let floor = quad ~material:checker P3.origin V3.unit_x V3.unit_z in
   let ceiling = quad ~material:grey (P3.of_v3 V3.unit_y) V3.unit_x V3.unit_z in
   let rear_wall = quad ~material:grey (P3.of_v3 V3.unit_z) V3.unit_x V3.unit_y in
   List.concat_no_order [ right_wall; left_wall; floor; ceiling; rear_wall ]
 ;;
 
-let camera ~aspect =
-  let eye = P3.create ~x:0.5 ~y:0.5 ~z:(-1.0) in
-  let target = P3.create ~x:0.5 ~y:0.5 ~z:0.0 in
-  let up = V3.unit_y in
-  Camera.create ~eye ~target ~up ~aspect ~vertical_fov_deg:45.0
+let spheres () =
+  let open Float.O in
+  let radius = 0.20 in
+  let metal =
+    let material = Material.metal (solid_tex 1.0 1.0 1.0) in
+    let center = P3.create ~x:(1.0 - 0.1 - radius) ~y:radius ~z:(1.0 - 0.2 - radius) in
+    Sphere.create ~material ~center ~radius
+  in
+  let glass =
+    let material = Material.glass in
+    let center = P3.create ~x:(0.1 + radius) ~y:radius ~z:(0.2 + radius) in
+    Sphere.create ~material ~center ~radius
+  in
+  [ metal; glass ]
 ;;
 
+module Shape = struct
+  type t =
+    | S of Sphere.t
+    | T of Triangle.t
+
+  let of_triangle t = T t
+  let of_sphere s = S s
+
+  module Hit = struct
+    type t =
+      | S of
+          { t_hit : float
+          ; sphere : Sphere.t
+          }
+      | T of Triangle.Hit.t
+
+    let t_hit = function
+      | S { t_hit; _ } -> t_hit
+      | T t -> Triangle.Hit.t_hit t
+    ;;
+
+    let to_scatter t ray =
+      match t with
+      | S { t_hit; sphere } -> Sphere.hit sphere t_hit ray
+      | T tri_hit ->
+        let open Float.O in
+        let module H = Triangle.Hit in
+        let g_normal = H.g_normal tri_hit in
+        let pt = H.point tri_hit in
+        let tex_coord = H.tex_coord tri_hit in
+        let hit_front = V3.dot (Ray.direction ray) g_normal < 0.0 in
+        let normal = if hit_front then g_normal else V3.Infix.( ~- ) g_normal in
+        let ss = Shader_space.create normal pt in
+        let wi = Shader_space.omega_i ss ray in
+        let material = Triangle.material @@ H.face tri_hit in
+        let do_scatter = Material.scatter material ss tex_coord ~omega_i:wi ~hit_front in
+        { Hit.shader_space = ss; emit = Color.black; do_scatter }
+    ;;
+  end
+
+  let bbox = function
+    | S s -> Sphere.bbox s
+    | T t -> Triangle.bbox t
+  ;;
+
+  let intersect t ray ~t_min ~t_max =
+    match t with
+    | S s ->
+      Option.map (Sphere.intersect s ray ~t_min ~t_max) ~f:(fun t_hit ->
+          Hit.S { t_hit; sphere = s })
+    | T t -> Option.map (Triangle.intersect t ray ~t_min ~t_max) ~f:(fun h -> Hit.T h)
+  ;;
+
+  let transform t ~f =
+    match t with
+    | S s -> S (Sphere.transform s ~f)
+    | T t -> T (Triangle.transform t ~f)
+  ;;
+end
+
 module Shape_tree :
-  Shape_tree.S with type elt := Triangle.t and type elt_hit := Triangle.Hit.t =
+  Shape_tree.S with type elt := Shape.t and type elt_hit := Shape.Hit.t =
 Shape_tree.Make (Shape_tree.Array_leaf (struct
-  include Triangle
+  include Shape
 
   type hit = Hit.t
 
@@ -77,10 +153,20 @@ end))
 
 let main argv =
   let { Render_command.Args.width; height; _ } = argv in
-  let camera = camera ~aspect:(width // height) in
+  let camera =
+    let aspect = width // height in
+    let eye = P3.create ~x:0.5 ~y:0.5 ~z:(-1.0) in
+    let target = P3.create ~x:0.5 ~y:0.5 ~z:0.0 in
+    let up = V3.unit_y in
+    Camera.create ~eye ~target ~up ~aspect ~vertical_fov_deg:45.0
+  in
   let tree =
     let f = Camera.transform camera in
-    Shape_tree.create @@ List.map (empty_box ()) ~f:(fun t -> Triangle.transform t ~f)
+    let shapes =
+      List.map (empty_box ()) ~f:Shape.of_triangle
+      @ List.map (spheres ()) ~f:Shape.of_sphere
+    in
+    Shape_tree.create @@ List.map shapes ~f:(fun t -> Shape.transform t ~f)
   in
   let module Render_cmd =
     Render_command.Make (struct
@@ -97,21 +183,7 @@ let main argv =
       let intersect ray =
         match Shape_tree.intersect tree ray ~t_min:0.0 ~t_max:Float.max_finite_value with
         | None -> None
-        | Some tri_hit ->
-          let open Float.O in
-          let module H = Triangle.Hit in
-          let g_normal = H.g_normal tri_hit in
-          let pt = H.point tri_hit in
-          let tex_coord = H.tex_coord tri_hit in
-          let hit_front = V3.dot (Ray.direction ray) g_normal < 0.0 in
-          let normal = if hit_front then g_normal else V3.Infix.( ~- ) g_normal in
-          let ss = Shader_space.create normal pt in
-          let wi = Shader_space.omega_i ss ray in
-          let material = Triangle.material @@ H.face tri_hit in
-          let do_scatter =
-            Material.scatter material ss tex_coord ~omega_i:wi ~hit_front
-          in
-          Some { Hit.shader_space = ss; emit = Color.black; do_scatter }
+        | Some h -> Some (Shape.Hit.to_scatter h ray)
       ;;
     end)
   in
