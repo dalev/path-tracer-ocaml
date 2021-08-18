fca140fba2ab2e375ef195b897e169fdc9a97b31
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-08-18 15:52:33 -0400

    mli

---
+++ cornell-box/bin/ppm.mli
@@ -0,0 +1,22 @@
+open Base
+
+module Point_light : sig
+  type t
+
+  val create : position:Path_tracer.Affine.p3 -> t
+end
+
+module Make (Scene : sig
+  val bbox : Path_tracer.Bbox.t
+  val camera : Path_tracer.Camera.t
+  val point_lights : Point_light.t list
+  val intersect : Path_tracer.Ray.t -> Path_tracer.Hit.t option
+  val width : int
+  val height : int
+  val max_bounces : int
+  val num_iterations : int
+  val photon_count : int
+  val alpha : float
+end) : sig
+  val go : Domainslib.Task.pool -> (float, Bigarray.float64_elt, [ `Rgb ]) Bimage.Image.t
+end
