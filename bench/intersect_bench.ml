f4c5ee98164eab08ab9662634b5355436afb60c9
Dale Vaillancourt <dalev@alum.wpi.edu>
2021-08-20 12:21:30 -0400

    Bbox.mem

--- bench/intersect_bench.ml
+++ bench/intersect_bench.ml
@@ -23,6 +23,8 @@ let is_hit ray =
   Bbox.is_hit (Sys.opaque_identity unit_bbox) (Sys.opaque_identity ray) ~t_min ~t_max
 ;;
 
+let bbox_mem b pt = Bbox.mem (Sys.opaque_identity b) (Sys.opaque_identity pt)
+
 let sphere_intersect s ray =
   Sphere.intersect (Sys.opaque_identity s) (Sys.opaque_identity ray) ~t_min ~t_max
   |> Option.is_some
@@ -32,12 +34,21 @@ let () =
   Core.Command.run
     (Bench.make_command
        [ Bench.Test.create_group
-           ~name:"bbox"
+           ~name:"bbox-intersect"
            [ Bench.Test.create ~name:"hit" (fun () -> assert (is_hit ray_hit))
            ; Bench.Test.create ~name:"miss" (fun () -> assert (not (is_hit ray_miss)))
            ]
        ; Bench.Test.create_group
-           ~name:"sphere"
+           ~name:"bbox-mem"
+           (let pt_in = P3.create ~x:0.5 ~y:0.5 ~z:0.5 in
+            let pt_out = P3.create ~x:2.0 ~y:(-0.5) ~z:0.0 in
+            [ Bench.Test.create ~name:"inside" (fun () ->
+                  assert (bbox_mem unit_bbox pt_in))
+            ; Bench.Test.create ~name:"outside" (fun () ->
+                  assert (not (bbox_mem unit_bbox pt_out)))
+            ])
+       ; Bench.Test.create_group
+           ~name:"sphere-intersect"
            [ Bench.Test.create ~name:"hit" (fun () ->
                  assert (sphere_intersect unit_sphere ray_hit))
            ; Bench.Test.create ~name:"miss" (fun () ->
