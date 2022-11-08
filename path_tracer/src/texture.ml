7ad553e953d68c5610ee4e6598ce7d1a27bbd7c9
Dale Vaillancourt <dalev@alum.wpi.edu>
2022-11-08 17:58:08 -0500

    refactor checker

--- path_tracer/src/texture.ml
+++ path_tracer/src/texture.ml
@@ -18,12 +18,14 @@ type t = Coord.t -> Color.t
 let eval t coord = t coord
 let solid c _ = c
 
-let checker ~width ~height even odd coord =
-  let { Coord.u; v } = coord in
-  let scale_coord c dim = Float.to_int (c *. Float.of_int (dim - 1)) in
-  let x' = scale_coord u width in
-  let y' = scale_coord v height in
-  let parity a = a land 1 in
-  let tex = if parity x' = parity y' then even else odd in
-  eval tex coord
+let checker ~width ~height even odd =
+  let width = Float.of_int (width - 1)
+  and height = Float.of_int (height - 1) in
+  let parity a = Float.to_int a land 1 in
+  fun coord ->
+    let { Coord.u; v } = coord in
+    let x' = u *. width
+    and y' = v *. height in
+    let tex = if parity x' = parity y' then even else odd in
+    eval tex coord
 ;;
