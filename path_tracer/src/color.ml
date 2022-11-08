ce763008d0f81769533159ba21cece8822f9998f
Dale Vaillancourt <dalev@alum.wpi.edu>
2022-11-08 16:51:26 -0500

    simplify color impl

--- path_tracer/src/color.ml
+++ path_tracer/src/color.ml
@@ -1,16 +1,8 @@
-type t = V3.t
+include V3
 
 let create ~r ~g ~b = V3.create ~x:r ~y:g ~z:b
 let to_rgb { V3.x; y; z } = x, y, z
 let of_v3 v = v
 let to_v3 v = v
-let black = create ~r:0.0 ~g:0.0 ~b:0.0
-let white = create ~r:1.0 ~g:1.0 ~b:1.0
-let zero = black
-let map = V3.map
-let fma = V3.fma
-let scale = V3.scale
-let lerp = V3.lerp
-let max_coord = V3.max_coord
-
-module Infix = V3.Infix
+let black = zero
+let white = one
