c1e0a8fea3afbfb4f18efcd35f3c9d607871b00d
Dale Vaillancourt <dalev@alum.wpi.edu>
2022-11-13 21:20:48 -0500

    .

--- path_tracer/src/axis.ml
+++ path_tracer/src/axis.ml
@@ -4,12 +4,10 @@ type t =
   | Z
 
 let all = [ X; Y; Z ]
-let first (a, _, _) = a
-let second (_, b, _) = b
-let third (_, _, c) = c
 
-let tuple_selector = function
-  | X -> first
-  | Y -> second
-  | Z -> third
+let tuple_selector t (a, b, c) =
+  match t with
+  | X -> a
+  | Y -> b
+  | Z -> c
 ;;
